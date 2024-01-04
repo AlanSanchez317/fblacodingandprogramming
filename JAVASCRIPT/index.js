9f31c238c181e0d60a064eb86ecb255141ea17ae
DerpNerd <asanch317@west-mec.org>
2024-01-04 16:43:49 -0700

    Fixed CSS and Finished Home Page

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -14,3 +14,27 @@ document.addEventListener('DOMContentLoaded', function () {
     window.addEventListener('scroll', fadeInElements);
     fadeInElements(); // Initial check
 });
+
+let UserCreds = JSON.parse(sessionStorage.getItem("user-creds"));
+let UserInfo = JSON.parse(sessionStorage.getItem("user-info"));
+
+let GreetHead = document.getElementById('greet');
+let SignoutBtn = document.getElementById('signoutbutton');
+
+
+let Signout = () => {
+    sessionStorage.removeItem("user-creds");
+    sessionStorage.removeItem("user-info");
+    window.location.href = './HTML/login.html'
+}
+
+let CheckCred = () => {
+    if (!sessionStorage.getItem("user-creds")) {
+        window.location.href = './HTML/login.html';
+    } else {
+        GreetHead.innerText = `Welcome ${UserInfo.firstname} ${UserInfo.lastname}!`;
+    }
+}
+
+window.addEventListener('load', CheckCred);
+SignoutBtn.addEventListener('click', Signout);
